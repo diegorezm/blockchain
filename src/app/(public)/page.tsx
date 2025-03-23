e2994268756721a2fff18211f82d7c6967ff318f
diegorezm <dmourarezende@gmail.com>
2025-03-23 11:08:13 -0300

    using clerk auth

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Clock, Coins, Shield } from "lucide-react";
 import { Card } from "@/components/ui/card";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
 
-export default function Home() {
+export default async function HomePage() {
+  const { userId } = await auth();
+
   const features = [
     {
       Icon: Shield,
@@ -25,20 +29,24 @@ export default function Home() {
     },
   ] as const;
   return (
-    <div className="flex flex-col items-center gap-40 pb-4 mt-12">
-      <section className="flex flex-col md:flex-row justify-between items-center h-full w-full px-6 md:px-24">
-        <div className="flex flex-col space-y-6 max-w-2xl items-center lg:items-start">
-          <h1 className="text-4xl md:text-5xl leading-tight font-bold text-center lg:text-left">
+    <div className="flex flex-col items-center gap-40 pb-4">
+      <section className="flex flex-col justify-between items-center h-screen w-full px-6 md:px-24 py-4 2xl:py-24">
+        <div className="flex flex-col space-y-6 items-center max-w-4xl text-center">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl leading-tight font-bold text-center lg:text-left">
             Welcome to <span className="text-primary">crypto</span>bank
           </h1>
-          <p className="text-lg text-center lg:text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-            incidunt assumenda provident deserunt voluptatibus. Voluptates?
+          <p className="text-lg text-surface">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
+            ipsa quos et corporis nisi cupiditate quo aperiam? Possimus quod ex
+            ullam, quasi, cumque ad, repellat provident illo totam veritatis
+            cum!
           </p>
           <div className="flex space-x-4">
-            <Button variant="primary" size="lg">
-              Get Started
-            </Button>
+            <Link href="/dashboard">
+              <Button variant="primary" size="lg">
+                {userId ? "Dashboard" : "Get started!"}
+              </Button>
+            </Link>
             <Button variant="outline">Learn More</Button>
           </div>
         </div>
